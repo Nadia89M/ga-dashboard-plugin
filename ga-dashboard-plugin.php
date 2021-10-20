a499af22798f4e45f4a65d5331b503816fe19012
Nadia <nadia.mohamed89@gmail.com>
2021-10-20 18:35:57 +0200

    first commit

---
+++ ga-dashboard-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  GA Dashboard Plugin
+Description:  GA Dashboard Plugin is a plugin that help administrators integrate GA
+Version:      1.0
+Author:       Nadia
+Author URI:   https://nadiamohamed.me
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  ga-dashboard
+Domain Path:  /languages
+*/
